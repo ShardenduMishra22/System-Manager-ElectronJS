c149c2d3d61a4be7086f61895135e8fb84cb4775
Shardendu Mishra <shardendumishrabrave@gmail.com>
2025-06-20 15:27:19 +0530

    refactor: simplify main window creation and remove unused code

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,68 +1,12 @@
 import { app, BrowserWindow, Menu } from 'electron';
-import { ipcMainHandle, ipcMainOn, isDev } from './util.js';
-import { getStaticData, pollResources } from './resourceManager.js';
-import { getPreloadPath, getUIPath } from './pathResolver.js';
-import { createTray } from './tray.js';
-import { createMenu } from './menu.js';
+import { isDev } from './util.js';
+import path from 'path';
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({
-    webPreferences: {
-      preload: getPreloadPath(),
-    },
-    // disables default system frame (dont do this if you want a proper working menu bar)
-    frame: false,
-  });
+  const mainWindow = new BrowserWindow({})
   if (isDev()) {
     mainWindow.loadURL('http://localhost:5123');
   } else {
-    mainWindow.loadFile(getUIPath());
+    mainWindow.loadFile(path.join(app.getAppPath(),"/dist/react-react/index.html"));
   }
-
-  pollResources(mainWindow);
-
-  ipcMainHandle('getStaticData', () => {
-    return getStaticData();
-  });
-
-  ipcMainOn('sendFrameAction', (payload) => {
-    switch (payload) {
-      case 'CLOSE':
-        mainWindow.close();
-        break;
-      case 'MAXIMIZE':
-        mainWindow.maximize();
-        break;
-      case 'MINIMIZE':
-        mainWindow.minimize();
-        break;
-    }
-  });
-
-  createTray(mainWindow);
-  handleCloseEvents(mainWindow);
-  createMenu(mainWindow);
 });
-
-function handleCloseEvents(mainWindow: BrowserWindow) {
-  let willClose = false;
-
-  mainWindow.on('close', (e) => {
-    if (willClose) {
-      return;
-    }
-    e.preventDefault();
-    mainWindow.hide();
-    if (app.dock) {
-      app.dock.hide();
-    }
-  });
-
-  app.on('before-quit', () => {
-    willClose = true;
-  });
-
-  mainWindow.on('show', () => {
-    willClose = false;
-  });
-}
